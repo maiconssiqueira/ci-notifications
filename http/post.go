5de30d4b11a8ef9f4ee772eca1931bf470f9a728
Maicon Siqueira <maicon.siqueira@live.com>
2022-10-29 14:49:38 -0300

    first commit

---
+++ http/post.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+)
+
+func HttpPost(payload []byte, url string, token string) []byte {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/json")
+
+	defer req.Body.Close()
+
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, _ := ioutil.ReadAll(res.Body)
+	return data
+}
