51490f49e1c0ca16ab24edf7e47d90894ee80068
Maicon Siqueira <maicon.siqueira@live.com>
2022-10-30 18:45:30 -0300

    chore: included pull request comment

--- cmd/comments.go
+++ cmd/comments.go
@@ -7,11 +7,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// commentsCmd represents the comments command
 var commentsCmd = &cobra.Command{
 	Use:   "comments",
-	Short: "",
-	Long:  ``,
+	Short: "The  comments supports send comment on pull requests",
 	Run: func(cmd *cobra.Command, args []string) {
 		res, err := github.Comment(pullrequest, message)
 		if err != nil {
